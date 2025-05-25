a45f97967527be44f99c8e7de7628d9b01d47afd
airifox <airifox@gmail.com>
2025-05-25 16:33:39 -0700

    E: sys functionality + final formatting touches

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
+import sys
 from stats import get_num_words
 from stats import get_char_count
 from stats import get_sorted_chars
 
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-    return file_contents
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
 
-def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    word_count = get_num_words(book_text)
-    char_count = get_char_count(book_text)
-    sorted_count = get_sorted_chars(char_count)
-    print(f"""============ BOOKBOT ============
-Analyzing book...
+    def get_book_text(filepath):
+        with open(filepath) as f:
+            file_contents = f.read()
+        return file_contents
+
+    def main():
+        book_text = get_book_text(sys.argv[1])
+        book_title = sys.argv[1]
+        word_count = get_num_words(book_text)
+        char_count = get_char_count(book_text)
+        sorted_count = get_sorted_chars(char_count)
+        print(f"""============ BOOKBOT ============
+Analyzing {book_title}...
 ----------- Word Count ----------
 Found {word_count} total words!
---------- Character Count -------
-{sorted_count}""")
+--------- Character Count -------""")
+        for char, count in sorted_count:
+            print(f"{char}: {count}")
 
 main()
