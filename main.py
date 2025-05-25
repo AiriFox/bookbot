e2916108629634c85b6e1778e28cf9ba1179539b
airifox <airifox@gmail.com>
2025-05-25 12:41:14 -0700

    D: added sort characters + formatting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_char_count
+from stats import get_sorted_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,6 +11,12 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = get_num_words(book_text)
     char_count = get_char_count(book_text)
-    print(char_count)
+    sorted_count = get_sorted_chars(char_count)
+    print(f"""============ BOOKBOT ============
+Analyzing book...
+----------- Word Count ----------
+Found {word_count} total words!
+--------- Character Count -------
+{sorted_count}""")
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,14 @@ def get_char_count(text):
         else:
             char_dict[char] = 1
     return char_dict
+
+def get_sorted_chars(char_dict):
+    listed_dict = list(char_dict.items())
+    only_letters = []
+    for char in listed_dict:
+        if char[0].isalpha():
+            only_letters.append(char)
+    sorted_list = sorted(only_letters, reverse=True, key=lambda item: item [1])
+    return sorted_list
+
+    
